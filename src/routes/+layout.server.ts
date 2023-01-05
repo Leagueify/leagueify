fde4d5abfbebad5f2e26c87e264e3e33b36601bd
Michael duBois <mdubois@bytement.com>
2023-01-04 22:35:57 -0700

    Add Database Integration to Installation Workflow

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,26 +3,39 @@ import { get } from "svelte/store";
 
 import type { LayoutServerLoad } from "./$types";
 
+import * as database from "$lib/server/database";
+import { League } from "$lib/server/models/league";
 import { leagueify } from "$lib/stores";
 
 export const load: LayoutServerLoad = async ({ url }) => {
-  // Check Leagueify Installation
-  // && url.pathname !== "/register" is temporary until database is connected
-  console.log(`Checking Leagueify Installation... ${installed()}`)
-  if (!installed() && url.pathname !== "/register") {
-    console.log(`${url.pathname}::Need to Install Leagueify.`);
+  const activeLeague = await getLeague();
+  const isInstalled = installed();
+
+  // Redirect to register if not installed
+  if (!isInstalled && !activeLeague && url.pathname !== "/register") {
     throw redirect(302, "/register");
   }
 
-  // Install Leagueify
-  // && url.pathname !== "/register" is temporary until database is connected
-  if (url.pathname === "/register") {
-    console.log("Installing Leagueify...");
+  // When a League is created mark as installed
+  if (activeLeague) {
+    // Install Leagueify
     leagueify.install();
   }
 };
 
+async function getLeague() {
+  const db = await database.connect();
+  const leagueData = await League.findOne({}).exec();
+
+  await database.disconnect(db);
+
+  if (leagueData) {
+    return true;
+  }
+
+  return false;
+}
 
 function installed() {
-  return get(leagueify)
+  return get(leagueify);
 }
