46923671148f48f167855be0513c8ac143f2aa1c
Michael duBois <mdubois@bytement.com>
2023-01-04 23:30:42 -0700

    Update /register Imports and Add Initial User

---
+++ src/lib/components/forms/user.svelte
@@ -0,0 +1,47 @@
+<div class="mb-3">
+  <h2>User</h2>
+  <div class="row g-2">
+    <div class="col-md">
+      <div class="input-group mb-3">
+        <div class="form-floating mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingInputGrid"
+            placeholder="First Name"
+          />
+          <label for="floatingInput">First Name</label>
+        </div>
+      </div>
+    </div>
+    <div class="col-md">
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="floatingInputGrid"
+          placeholder="Last Name"
+        />
+        <label for="floatingInput">Last Name</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-floating mb-3">
+    <input
+      type="email"
+      class="form-control"
+      id="floatingInput"
+      placeholder="example@leagueify.org"
+    />
+    <label for="floatingInput">Email Address</label>
+  </div>
+  <div class="form-floating mb-3">
+    <input
+      type="tel"
+      class="form-control"
+      id="floatingInput"
+      placeholder="example@leagueify.org"
+    />
+    <label for="floatingInput">Phone Number</label>
+  </div>
+</div>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-  import { leagueify } from "$lib/stores";
-
   import League from "$lib/components/forms/league.svelte";
+  import User from "$lib/components/forms/user.svelte";
+  import { leagueify } from "$lib/stores";
 </script>
 
+// Fix Bux with If Statement - It shows when league present
 {#if !$leagueify}
   <League />
 {/if}
+<User />
