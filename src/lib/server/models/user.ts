e8f138410290e36a3286bf42fde1e85183b4c92e
Michael duBois <mdubois@bytement.com>
2023-01-07 01:44:02 -0700

    Fix BigInt is not a valid Type

--- src/lib/server/models/user.ts
+++ src/lib/server/models/user.ts
@@ -5,7 +5,7 @@ interface IUser {
   lastName: string;
   email: string;
   phone: string;
-  dateOfBirth: bigint;
+  dateOfBirth: number;
   players: string[];
   coach: boolean;
   volunteer: boolean;
@@ -13,7 +13,7 @@ interface IUser {
   password: string;
   salt: string;
   token: string;
-  tokenExpiration: bigint;
+  tokenExpiration: number;
 }
 
 enum Role {
@@ -27,7 +27,7 @@ const schema = new Schema<IUser>({
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
-  dateOfBirth: { type: BigInt, required: true },
+  dateOfBirth: { type: Number, required: true },
   players: { type: [String], required: true },
   coach: { type: Boolean, required: true },
   volunteer: { type: Boolean, required: true },
@@ -35,7 +35,7 @@ const schema = new Schema<IUser>({
   password: { type: String, required: true },
   salt: { type: String, required: true },
   token: { type: String, required: false },
-  tokenExpiration: { type: BigInt, required: false },
+  tokenExpiration: { type: Number, required: false },
 });
 
 const User = model<IUser>("User", schema);
