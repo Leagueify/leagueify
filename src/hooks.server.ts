fe0ce473fc28e1cf9db5a0c632ac831bb2220264
Michael duBois <mdubois@bytement.com>
2023-01-09 21:01:16 -0700

    Add Handle Function to Server Hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,11 @@
 import * as Sentry from "@sentry/node";
 import "@sentry/tracing";
+import { get } from "svelte/store";
 
-import type { HandleServerError } from "@sveltejs/kit";
+import type { Handle, HandleServerError } from "@sveltejs/kit";
 
 import { PUBLIC_SENTRY_DSN, PUBLIC_SENTRY_ENV } from "$env/static/public";
+import { leagueData } from "$lib/stores";
 
 if (PUBLIC_SENTRY_DSN) {
   Sentry.init({
@@ -12,6 +14,29 @@ if (PUBLIC_SENTRY_DSN) {
     tracesSampleRate: 1.0,
   });
 }
+
+export const handle = (async ({ event, resolve }) => {
+  if (!get(leagueData).name) {
+    console.log("No Data")
+  }
+
+  console.log(get(leagueData).name)
+
+  leagueData.update((data) => {
+    return {
+      ...data,
+      name: "League Name",
+    };
+  });
+
+
+  if (event.url.pathname.startsWith('/custom')) {
+    return new Response('custom response');
+  }
+
+  const response = await resolve(event);
+  return response;
+}) satisfies Handle;
 
 export const handleError = (({ error, event }) => {
   if (PUBLIC_SENTRY_DSN) {
