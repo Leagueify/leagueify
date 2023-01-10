9c480c9da52aba105937bb32135c0a0e280e26dc
Michael duBois <mdubois@bytement.com>
2023-01-09 21:00:29 -0700

    Create leagueData Store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,12 +1,3 @@
 import { writable } from "svelte/store";
 
-function leagueifyInstallation() {
-  const { subscribe, set } = writable(false);
-
-  return {
-    subscribe,
-    install: () => set(true),
-  };
-}
-
-export const leagueify = leagueifyInstallation();
+export const leagueData = writable({});
