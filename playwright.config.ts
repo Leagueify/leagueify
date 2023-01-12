effcc78de337d97e9cc8fecbedddc0300f8b055a
Michael duBois <mdubois@bytement.com>
2023-01-11 18:50:26 -0700

    Update Playwright Config Command and Port

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,8 +11,8 @@ const config: PlaywrightTestConfig = {
     ],
   ],
   webServer: {
-    command: "npm run build && npm run preview",
-    port: 4173,
+    command: "npm run dev",
+    port: 3000,
   },
   testDir: "tests",
 };
