33a33e4e3fc72d69c0f7583e9333002c84bb5764
Michael duBois <mdubois@bytement.com>
2023-01-11 21:19:29 -0700

    Restore Default Test

--- tests/test.ts
+++ tests/test.ts
@@ -2,6 +2,5 @@ import { expect, test } from "@playwright/test";
 
 test("index page has expected h1", async ({ page }) => {
   await page.goto("/");
-  expect(page.url()).toBe("http://localhost:3000/register");
-  // expect(await page.textContent("h1")).toBe("Welcome to SvelteKit");
+  expect(await page.textContent("h1")).toBe("Welcome to SvelteKit");
 });
