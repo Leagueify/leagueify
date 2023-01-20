c07c368f840b153ff2feb1015fde3d149ce04880
Michael duBois <mdubois@bytement.com>
2023-01-20 16:23:58 -0700

    Update Tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,12 @@
 import { expect, test } from "@playwright/test";
 
-test("index page has expected h1", async ({ page }) => {
+test("Redirect to /register when league is not installed", async ({ page }) => {
   await page.goto("/");
-  expect(await page.textContent("h1")).toBe("Welcome to SvelteKit");
+  expect(page.url()).toContain("/register");
 });
+
+test("League form is present", async ({ page }) => {
+  await page.goto("/");
+  expect(await page.isVisible("#leagueName")).toBe(true);
+  expect(await page.isVisible("#leagueSport")).toBe(true);
+})
