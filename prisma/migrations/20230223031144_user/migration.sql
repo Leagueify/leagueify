7a3f1045872c21737ef2f42fc8d8718afeaf63bc
Michael duBois <mdubois@bytement.com>
2023-02-22 20:12:01 -0700

    Update Migrations

--- prisma/migrations/20230223031144_user/migration.sql
+++ prisma/migrations/20230223031144_user/migration.sql
@@ -4,12 +4,11 @@ CREATE TYPE "Role" AS ENUM ('MASTER_ADMIN', 'ADMIN', 'USER');
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL,
-    "phone" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(32) NOT NULL,
+    "email" VARCHAR(64) NOT NULL,
+    "phone" VARCHAR(25) NOT NULL,
     "birthdate" DATE NOT NULL,
     "password" VARCHAR(255) NOT NULL,
-    "salt" VARCHAR(255) NOT NULL,
     "role" "Role" NOT NULL DEFAULT 'USER',
     "token" VARCHAR(255) NOT NULL,
     "expiration" DATE NOT NULL,
