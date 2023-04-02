5e2ce4a0eea9257bbe49845f122107c7291ce0ca
Michael duBois <mdubois@bytement.com>
2023-04-02 14:54:42 -0600

    Add Page Handling for /register

---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import { Stepper, Step } from "@skeletonlabs/skeleton";
+  import { enhance } from "$app/forms";
+
+  import { formData, leagueStore } from "$lib/stores";
+
+  import Account from "$lib/components/forms/account.svelte";
+  import EmailConfig from "$lib/components/forms/emailConfig.svelte";
+  import League from "$lib/components/forms/league.svelte";
+
+  export let data: object;
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+  <form
+    method="post"
+    novalidate
+    use:enhance={({ data }) => {
+      console.log($formData);
+      for (const [key, value] of Object.entries($formData)) {
+        data.set(key, value);
+      }
+    }}>
+    <Stepper
+      buttonCompleteLabel="Submit"
+      buttonCompleteType="submit"
+      buttonNextType="button">
+      <!-- Only show if Leagueify is not installed -->
+      {#if !$leagueStore.installed}
+        <Step>
+          <span slot="header">Welcome to Leagueify!</span>
+          We will now guide you through the process of installing Leagueify.
+        </Step>
+        <League {data} />
+        <EmailConfig />
+      {/if}
+      <Account />
+    </Stepper>
+  </form>
+</div>
