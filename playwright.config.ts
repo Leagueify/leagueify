5ae9fe1e177273b95abfb173ab5f57d855c82db1
Michael duBois <mdubois@bytement.com>
2023-04-03 21:14:38 -0600

    Fix Redirection Tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,8 +11,8 @@ const config: PlaywrightTestConfig = {
     ],
   ],
   webServer: {
-    command: "npm run build && npm run preview",
-    port: 4173,
+    command: "npm run dev -- --host --port=3000",
+    port: 3000,
   },
   testDir: "tests",
 };
