485b347c99f6dcbb6a15abe6f8a1d73d457b389a
Michael duBois <mdubois@bytement.com>
2023-06-07 18:53:02 -0600

    Update Svelte Stores

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,10 +1,48 @@
+// 3rd Party Imports
 import { writable } from "svelte/store";
+// Type Imports
+import type { PageData } from "./types";
 
-const forms = {};
+interface Divisions {
+  id?: number;
+  name?: string;
+  minAge?: number;
+  maxAge?: number;
+}
 
-const league = {
+interface League {
+  installed: boolean;
+  id?: number;
+  name?: string;
+  domain?: string;
+  leagueAdmin?: number;
+  emailConfig?: number;
+}
+
+interface Positions {
+  id?: number;
+  name?: string;
+}
+
+interface User {
+  id?: number;
+  name?: string;
+  email?: string;
+  token?: string;
+}
+
+// Store Data
+const divisions: Divisions[] = [];
+const forms: PageData = {};
+const league: League = {
   installed: false,
 };
+const positions: Positions[] = [];
+const user: User = {};
 
-export const formData = writable(forms);
+// Export Stores
+export const divisionsStore = writable(divisions);
+export const formStore = writable(forms);
 export const leagueStore = writable(league);
+export const positionsStore = writable(positions);
+export const userStore = writable(user);
