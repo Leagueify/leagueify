d867ee2ae713b6fe6f6606d58213980a66bfe34c
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-09-17 22:25:53 -0600

    rudementary validation on league details

--- src/lib/components/forms/leagueDetails.svelte
+++ src/lib/components/forms/leagueDetails.svelte
@@ -5,11 +5,24 @@
   import type { PageData } from "./$types";
   // Leagueify Imports
   import { formStore } from "$lib/stores";
+    import SportDetails from './sportDetails.svelte';
   // Variables
   export let data: PageData = {};
+  
+  let leagueNameCheck = true;
+  let leagueCheck = true;
+  
+  let onInput = (e: any) => leagueNameCheck =  e.target.value.length >= 1 ? false : true;
+
+  // let onLeagueSelect = (e: any) => leagueCheck = e.target.value !== 'none' ? false : true;
+  let onLeagueSelect = (e: any) => leagueCheck = e.target.vaue !== "none" ? false : true;
+
+  $: formLocked = leagueNameCheck === true || leagueCheck === true ? true : false
+
+
 </script>
 
-<Step>
+<Step locked={formLocked}>
   <span slot="header">League Details</span>
   <span
     >This is where you will create your league. Enter the name of your league,
@@ -23,6 +36,7 @@
       id="leagueName"
       placeholder="League Name"
       maxlength="64"
+      on:input={onInput}
       bind:value={$formStore.leagueName} />
   </label>
   <label class="label">
@@ -30,6 +44,7 @@
     <select
       class="select variant-form-material"
       name="leagueSport"
+      on:focus={onLeagueSelect}
       bind:value={$formStore.leagueSport}>
       <option
         value="none"
